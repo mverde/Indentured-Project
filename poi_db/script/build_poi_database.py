56fa2ca9c4b2e1a5b42da0c58135cd18bdbe4789
brmi <desireelenart@gmail.com>
2018-02-15 21:21:01 -0800

    Put in an algorithm for breaking up the search areas for a certain radius. Still need to test to see if it is accurate

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -3,29 +3,38 @@ import googlemaps
 
 gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
-###places_dict = gmaps.places('', (34.0537136,-118.24265330000003), 10000)
-# places_dict = gmaps.places_nearby(location=(34.0537136,-118.24265330000003), radius=1)
-# print places_dict['results']
+def milesToMeters(miles):
+    return miles*1609.34
 
-#dummy data
-# dummyPlace = {
-#     place: 'UCLA',
-#     latitude: 34.05,
-#     longitude: -118,
-#     typeOfPlace: 'School' 
-# }
- 
-def getLocations(coordinates, rad):
-    # input: coordinates (latitude,longitude), radius
-    # output: dict of locations
-    return  gmaps.places_nearby(location=coordinates, radius=rad)
+# Globals
+defaultSearchRadiusInMiles = 0.25 
+defaultSearchRadius = milesToMeters(defaultSearchRadiusInMiles)
+degreeDelta = 10
 
-def searchAreaAlgorithm(start):
+def searchAreaAlgorithm(startLat, startLong, radius):
     # input: start coordinate 
     # only returns 60 places.. then gotta go on to next place ... how far should we spread each search??
-    print 'hi'
-    
+    # lets do every 1 mile for now ...
+    # when to stop??? well when ur out of radius
+    # so given start is the center.. think of unit circle.. 
 
+    count = 0
+    degree = 0
+    searchResults = []
+    rad = milesToMeters(radius)
+
+    # 360 degrees in a circle, move by 1 degree each time
+    while(degree < 360):
+        currentPoint = 0
+        while(currentPoint < rad):
+            #search spot
+            searchResults.append((gmaps.places_nearby(location=(startLat, startLong), radius=defaultSearchRadius))['results'])
+            currentPoint = currentPoint + defaultSearchRadius*2
+            print ("current point: ", currentPoint)
+        degree += degreeDelta
+        print ("degree: ", degree)
+    
+    return searchResults
 
 def addToDB(place, latitude, longitude, typeOfPlace):
     #input: locations array - add to db all at once    
@@ -34,9 +43,9 @@ def addToDB(place, latitude, longitude, typeOfPlace):
 
 
 def main():
-    print getLocations((34.0537136,-118.24265330000003), 1)['results'];
-    # addToDB(dummyPlace.place, dummyPlace.latitude, dummyPlace.longitude, dummyPlace.typeOfPlace)
-
+    # print getLocations((34.0537136,-118.24265330000003), 1)['results'];
+    print searchAreaAlgorithm(34, -118, 1)
+    
 
 main()
 
