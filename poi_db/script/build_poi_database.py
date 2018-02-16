7a9a40db0bac0f2da79047dfa11778bf44580d5b
Melissa Chen <melissachen2010@hotmail.com>
2018-02-15 23:25:01 -0800

    addToDB: adds place, lat, long. wasn't able to fully test lat/long yet though

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -1,51 +1,63 @@
 import googlemaps
-# import mysql.connector
-
+import MySQLdb
 gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
-def milesToMeters(miles):
-    return miles*1609.34
+###places_dict = gmaps.places('', (34.0537136,-118.24265330000003), 10000)
+# places_dict = gmaps.places_nearby(location=(34.0537136,-118.24265330000003), radius=1)
+# print places_dict['results']
 
-# Globals
-defaultSearchRadiusInMiles = 0.25 
-defaultSearchRadius = milesToMeters(defaultSearchRadiusInMiles)
-degreeDelta = 10
+ 
+def getLocations(coordinates, rad):
+    # input: coordinates (latitude,longitude), radius
+    # output: dict of locations
+    return  gmaps.places_nearby(location=coordinates, radius=rad)
 
-def searchAreaAlgorithm(startLat, startLong, radius):
-    # input: start coordinate 
-    # only returns 60 places.. then gotta go on to next place ... how far should we spread each search??
-    # lets do every 1 mile for now ...
-    # when to stop??? well when ur out of radius
-    # so given start is the center.. think of unit circle.. 
+# def searchAreaAlgorithm(start):
+#     # input: start coordinate 
 
-    count = 0
-    degree = 0
-    searchResults = []
-    rad = milesToMeters(radius)
-
-    # 360 degrees in a circle, move by 1 degree each time
-    while(degree < 360):
-        currentPoint = 0
-        while(currentPoint < rad):
-            #search spot
-            searchResults.append((gmaps.places_nearby(location=(startLat, startLong), radius=defaultSearchRadius))['results'])
-            currentPoint = currentPoint + defaultSearchRadius*2
-            print ("current point: ", currentPoint)
-        degree += degreeDelta
-        print ("degree: ", degree)
-    
-    return searchResults
-
-def addToDB(place, latitude, longitude, typeOfPlace):
+def addToDB(array):
     #input: locations array - add to db all at once    
     # db columns: index, place, coordinate(lat, longitude), type
-    print (place, latitude, longitude, typeOfPlace)
 
+    # ================= Connect to DB ================= #
+    db = MySQLdb.connect(host= "localhost",
+                    user="root",
+                    passwd="password")
+    cursor = db.cursor()
+    # cursor.execute("SET sql_notes = 0; ")       # Suppress warning
+
+    # ================= Default database and table set up ================= #
+    cursor.execute('CREATE DATABASE IF NOT EXISTS pois');
+    cursor.execute('USE pois')
+    cursor.execute("DROP TABLE IF EXISTS test")
+    cursor.execute("CREATE TABLE test (place VARCHAR(70), lat DECIMAL(10, 8) NOT NULL, lng DECIMAL(11, 8) NOT NULL)")
+
+    # ================= Parse array ================= #
+    for entry in array:
+            # print (entry)
+            # print ('\n')
+
+            # print ("*** NAME OF THING ***" + entry['name'])
+            placeName = entry['name']
+            lat = entry['geometry']['location']['lat']
+            long = entry['geometry']['location']['long']
+
+            try:
+                params = (placeName, lat, long)
+                cursor.execute("""
+                    INSERT INTO test 
+                    VALUES
+                        (%s, %s, %s)
+                        """, params)
+
+                db.commit()
+            except:
+                db.rollback()
+    db.close()
+    return
 
 def main():
-    # print getLocations((34.0537136,-118.24265330000003), 1)['results'];
-    print searchAreaAlgorithm(34, -118, 1)
-    
+    addToDB(getLocations((34.0537136,-118.24265330000003), 2)['results'])
 
 main()
 
