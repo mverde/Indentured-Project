8ff814d1ccc148e930519854262ee123813485a4
Martin Verde <martinthespartan23@gmail.com>
2018-02-16 00:01:51 -0800

    Removed duplicate line.

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -1,7 +1,6 @@
 import googlemaps
 import MySQLdb
 import math
-gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
 ###places_dict = gmaps.places('', (34.0537136,-118.24265330000003), 10000)
 # places_dict = gmaps.places_nearby(location=(34.0537136,-118.24265330000003), radius=1)
