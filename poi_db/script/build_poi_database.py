c72db4d0d849152be423f7d9ea73c73c13730182
brmi <desireelenart@gmail.com>
2018-02-15 20:30:43 -0800

    script runs now

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -1,4 +1,5 @@
 import googlemaps
+# import mysql.connector
 
 gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
@@ -6,6 +7,13 @@ gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 # places_dict = gmaps.places_nearby(location=(34.0537136,-118.24265330000003), radius=1)
 # print places_dict['results']
 
+#dummy data
+# dummyPlace = {
+#     place: 'UCLA',
+#     latitude: 34.05,
+#     longitude: -118,
+#     typeOfPlace: 'School' 
+# }
  
 def getLocations(coordinates, rad):
     # input: coordinates (latitude,longitude), radius
@@ -14,15 +22,21 @@ def getLocations(coordinates, rad):
 
 def searchAreaAlgorithm(start):
     # input: start coordinate 
+    # only returns 60 places.. then gotta go on to next place ... how far should we spread each search??
+    print 'hi'
+    
 
 
-def addToDB(place, coordinate, type):
+def addToDB(place, latitude, longitude, typeOfPlace):
     #input: locations array - add to db all at once    
     # db columns: index, place, coordinate(lat, longitude), type
+    print (place, latitude, longitude, typeOfPlace)
 
 
 def main():
-    print queryGoogle((34.0537136,-118.24265330000003), 1)['results'];
+    print getLocations((34.0537136,-118.24265330000003), 1)['results'];
+    # addToDB(dummyPlace.place, dummyPlace.latitude, dummyPlace.longitude, dummyPlace.typeOfPlace)
+
 
 main()
 
