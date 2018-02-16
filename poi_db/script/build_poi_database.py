c9e31e1c62b1a03371ef6fc13ae43dba8f03338e
Martin Verde <martinthespartan23@gmail.com>
2018-02-15 23:56:30 -0800

    Added functions to get new lat and long coordinates by adding meters to old lat and long.

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -1,6 +1,17 @@
 import googlemaps
 import math
 
+'''
+Global variables.
+'''
+DEFAULT_SEARCH_RADIUS_MILES = 0.25
+DEFAULT_SEARCH_RADIUS_METERS = milesToMeters(DEFAULT_SEARCH_RADIUS_MILES)
+EARTH_RADIUS_METERS = 6371000
+
+'''
+Google Maps Client interface object. In production, the API key should be
+stored locally.
+'''
 gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
 def milesToMeters(miles):
@@ -12,18 +23,34 @@ def metersToLatitude(meters):
 def metersToLongitude(meters, latitude):
     return 111320*math.cos(math.radians(latitude))*meters
 
-# Globals
-defaultSearchRadiusInMiles = 0.25 
-defaultSearchRadius = milesToMeters(defaultSearchRadiusInMiles)
-degreeDelta = 10
+'''
+Functions to get new latitude and longitude coordinates by adding meters to 
+old coordinates. This necessitates scaling and wrap-around handling for
+longitude conversions. Following established convention, we treat negative
+meters as going South/West and vice versa. Note that latPlusMeters does not
+handle situations where the new latitude would cross one of the poles.
+'''
+def latPlusMeters(latitude, meters):
+    return latitude + (meters / EARTH_RADIUS_METERS) * (180.0 / math.pi);
+
+def longPlusMeters(longitude, latitude, meters):
+    longToAdd = (meters / EARTH_RADIUS_METERS) * (180.0 / math.pi) / math.cos(latitude * math.pi / 180.0)
+    newLong = longitude + longToAdd
+
+    if newLong > 180.0:
+        return -180.0 + (newLong - 180.0)
+    elif newLong < -180.0:
+        return 180.0 - (newLong + 180.0)
+    
+    return newLong
 
 def searchAreaAlgorithm(latitude, longitude, radius):
     # input: starting & ending coordinates & radius of circle
     startLat = latitude + metersToLatitude(milesToMeters(radius))
     startLong = longitude + metersToLongitude(milesToMeters(radius))
     
-    currentLat = startLat + metersToLatitude(defaultSearchRadius)
-    currentLong = startLong - metersToLatitude(defaultSearchRadius)
+    currentLat = startLat + metersToLatitude(DEFAULT_SEARCH_RADIUS_METERS)
+    currentLong = startLong - metersToLatitude(DEFAULT_SEARCH_RADIUS_METERS)
     searchResults = []
 
     while(currentLong < endLong):  
