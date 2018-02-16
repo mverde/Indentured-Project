204dfb6545dfac68a1e28908ad5b957f07f3dce9
brmi <desireelenart@gmail.com>
2018-02-15 22:34:15 -0800

    Changed algorithm for searching through area based on a square.

--- poi_db/script/build_poi_database.py
+++ poi_db/script/build_poi_database.py
@@ -1,39 +1,36 @@
 import googlemaps
-# import mysql.connector
+import math
 
 gmaps = googlemaps.Client(key='AIzaSyBmvB1gGLH0cujfkhQylJu6St3BIqLcvwU')
 
 def milesToMeters(miles):
     return miles*1609.34
 
+def metersToLatitude(meters):
+    return meters/110540
+
+def metersToLongitude(meters, latitude):
+    return 111320*math.cos(math.radians(latitude))*meters
+
 # Globals
 defaultSearchRadiusInMiles = 0.25 
 defaultSearchRadius = milesToMeters(defaultSearchRadiusInMiles)
 degreeDelta = 10
 
-def searchAreaAlgorithm(startLat, startLong, radius):
-    # input: start coordinate 
-    # only returns 60 places.. then gotta go on to next place ... how far should we spread each search??
-    # lets do every 1 mile for now ...
-    # when to stop??? well when ur out of radius
-    # so given start is the center.. think of unit circle.. 
+def searchAreaAlgorithm(startLat, startLong, endLat, endLong, radius):
+    # input: starting & ending coordinates & radius of circle
 
-    count = 0
-    degree = 0
+    currentLat = startLat + metersToLatitude(defaultSearchRadius)
+    currentLong = startLong - metersToLatitude(defaultSearchRadius)
     searchResults = []
-    rad = milesToMeters(radius)
 
-    # 360 degrees in a circle, move by 1 degree each time
-    while(degree < 360):
-        currentPoint = 0
-        while(currentPoint < rad):
-            #search spot
-            searchResults.append((gmaps.places_nearby(location=(startLat, startLong), radius=defaultSearchRadius))['results'])
-            currentPoint = currentPoint + defaultSearchRadius*2
-            print ("current point: ", currentPoint)
-        degree += degreeDelta
-        print ("degree: ", degree)
-    
+    while(currentLong < endLong):  
+        while(currentLat < endLat):
+            searchResults.append((gmaps.places_nearby(location=(currentLat, currentLong), radius=defaultSearchRadius))['results'])
+            currentLat += metersToLatitude(defaultSearchRadius)*2
+        currentLong += metersToLatitude(defaultSearchRadius)*2
+        currentLat = startLat + metersToLongitude(defaultSearchRadius, metersToLatitude(currentLat))
+
     return searchResults
 
 def addToDB(place, latitude, longitude, typeOfPlace):
