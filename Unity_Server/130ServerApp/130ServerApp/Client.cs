b20c40249ac8e30f1f9466327170dc93d9f9b13a
hunterjg <hjgorczycki@yahoo.com>
2018-03-05 02:09:10 -0800

    Added Unity Integration and Server Component
    Added a C# server integration Visual Studio project that allows you to
    open ports and accept communications using the .NET protocol. Will most
    likely change this to C++ in the near future.
    
    Added a Unity Scene with the C# scripts necessary to make a TCP
    connection to the C# server. (Will be updated when server changed to
    C++)
    
    Most of this work was done offline over time and am only now uploading
    to Git.

---
+++ Unity_Server/130ServerApp/130ServerApp/Client.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace _130ServerApp
+{
+    class Client
+    {
+        public int Index;
+        public string IP;
+        public TcpClient Socket;
+        public NetworkStream myStream;
+        private byte[] readBuff;
+
+
+        public void Start()
+        {
+            Socket.SendBufferSize = 4096;
+            Socket.ReceiveBufferSize = 4096;
+            myStream = Socket.GetStream();
+            Array.Resize(ref readBuff, Socket.ReceiveBufferSize);
+            myStream.BeginRead(readBuff, 0, Socket.ReceiveBufferSize, OnReceiveData, null);
+        }
+
+        void CloseConnection()
+        {
+            Socket.Close();
+            Socket = null;
+            Console.WriteLine("User Disconnected: " + IP);
+        }
+
+        void OnReceiveData(IAsyncResult result)
+        {
+            try
+            {
+                int readBytes = myStream.EndRead(result);
+                if (Socket == null)
+                {
+                    return;
+                }
+                if (readBytes <= 0)
+                {
+                    CloseConnection();
+                    return;
+                }
+
+                byte[] newBytes = null;
+                Array.Resize(ref newBytes, readBytes);
+                Buffer.BlockCopy(readBuff, 0, newBytes, 0, readBytes);
+
+                //handledata
+
+                if (Socket == null)
+                {
+                    return;
+                }
+
+                myStream.BeginRead(readBuff, 0, Socket.ReceiveBufferSize, OnReceiveData, null);
+            }
+            catch(Exception ex)
+            {
+                CloseConnection();
+                return;
+            }
+
+        }//end onReceiveData
+
+
+
+
+
+
+    }//end Client Class
+}
---
+++ Unity_Server/130ServerApp/130ServerApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _130ServerApp
+{
+    class Program
+    {
+        private static Thread threadConsole;
+        private static bool consoleRunning;
+
+        static void Main(string[] args)
+        {
+            threadConsole = new Thread(new ThreadStart(ConsoleThread));
+            threadConsole.Start();
+            network.instance.ServerStart();
+        }
+
+        private static void ConsoleThread()
+        {
+            string line;
+            consoleRunning = true;
+
+            while(consoleRunning)
+            {
+                line = Console.ReadLine();
+
+                if(String.IsNullOrWhiteSpace(line))
+                {
+                    consoleRunning = false;
+                    return;
+                }
+                else
+                {
+
+                }
+
+            }
+          
+        }
+    }
+}
